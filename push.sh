f78f0355e7f8b3e7d296028834fba6fb60fb8f52
mdlEducation <46280750+mdlEducation@users.noreply.github.com>
2019-01-24 17:33:33 +0300

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "=====================================";
+echo "Start script";
+echo "=====================================";
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
